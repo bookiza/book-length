a423d382dee4915be47bbd3b75e990a794e05b23
Marvin Danig <marvin@marvindanig.com>
2016-06-07 23:00:43 -0400

    direct shell ls

--- length.js
+++ length.js
@@ -2,7 +2,7 @@ function length() {
     const shell = require('shelljs');
     const path = require('path');
 
-    return shell.exec( ls('-d', path.join('manuscript', 'page-*')).length );
+    return ls('-d', path.join('manuscript','page-*')).length;
 }
 
 module.exports.length = length;
